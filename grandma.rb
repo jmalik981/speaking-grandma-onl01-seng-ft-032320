9723fc4838ef0a660bf87ae74481c35c8cb71a88
Junaid Malik <jmalik981@gmail.com>
2020-04-12 07:15:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ phrase = "I LOVE YOU GRANDMA!"
 
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
  else puts "HUH?! SPEAK UP, SONNY!"
 end
 end
